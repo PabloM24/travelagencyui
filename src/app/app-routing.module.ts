42e610038907921193f0a5e897077694119abae1
PabloM24 <pama24@hotmail.es>
2020-11-29 22:15:57 -0600

    Working on base GUI 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HomeContentComponent } from './home-content/home-content.component';
 import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -5,15 +6,17 @@ import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
+export const routes: Routes = [
 
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'home', redirectTo: 'home/content', pathMatch: 'full' },
+  { path: 'home', redirectTo: 'home/homeContent', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'homeContent', component: HomeContentComponent },
   {
     path: 'home', component: HomeComponent, canActivate: [AuthGuard],
     children: [
+      { path: 'homeContent', component: HomeContentComponent }
     ]
   }
 ];
@@ -23,3 +26,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, LoginComponent, HomeContentComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents, routes } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
@@ -11,13 +12,16 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RecaptchaModule } from 'ng-recaptcha';
 import { AuthGuard } from './auth/auth.guard';
+import { HomeContentComponent } from './home-content/home-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    routingComponents,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    HomeContentComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +30,7 @@ import { AuthGuard } from './auth/auth.guard';
     FormsModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
+    RouterModule.forRoot(routes),
     RecaptchaModule
   ],
   providers: [AuthGuard],
